2a854c65d92e4dc0752e8c23ae920b000a8c9f13
SSE4 <tomskside@gmail.com>
2019-07-19 09:35:21 -0700

    - MSVC portability fixes
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -47,18 +47,63 @@ class GPGErrorConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
+    def _apply_msvc_fixes(self):
+        unistd_h = """
+#ifdef _MSC_VER
+#define access _access
+#define R_OK 4
+#define W_OK 2
+#define X_OK R_OK
+#define F_OK 0
+
+# define S_IRWXU    0700
+# define S_IRUSR    0400
+# define S_IWUSR    0200
+# define S_IXUSR    0100
+
+typedef int ssize_t;
+#else
+#include <unistd.h>
+#endif
+"""
+        os.makedirs(os.path.join(self._source_subfolder, "src", "sys"))
+        tools.save(os.path.join(self._source_subfolder, "src", "unistd.h"), unistd_h)
+        tools.save(os.path.join(self._source_subfolder, "src", "sys", "file.h"), unistd_h)
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "gpg-error.c"), '\x0c', '')
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "gpg-error.h.in"),
+                              "#undef GPGRT_HAVE_MACRO_FUNCTION",
+                              """#undef GPGRT_HAVE_MACRO_FUNCTION
+#ifdef _MSC_VER
+#define GPGRT_HAVE_MACRO_FUNCTION 1
+#endif
+""")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "gpg-error.c"),
+                              """#if HAVE_W32_SYSTEM
+                "Return the locale used for gettext"
+#else
+                "@"
+#endif""",
+                              '                "Return the locale used for gettext"')
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "Makefile.in"),
+                              "$(CPP) $(CPPFLAGS) $(extra_cppflags) -P _$@",
+                              "$(CPP) $(CPPFLAGS) $(extra_cppflags) -EP _$@")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "src", "w32-estream.c"),
+                              "#include <windows.h>", "#include <windows.h>\n" + unistd_h)
+
     def build(self):
+
         # the previous step might hang when converting from ISO-8859-2 to UTF-8 late in the build process
         os.unlink(os.path.join(self._source_subfolder, "po", "ro.po"))
         build = None
         host = None
         rc = None
+        env = dict()
         args = ["--disable-dependency-tracking",
                 "--disable-nls",
                 "--disable-languages",
                 "--disable-doc",
                 "--disable-tests"]
-        if self.settings.os != "Windows" and self.options.pic:
+        if self.settings.os != "Windows" and self.options.fPIC:
             args.append("--with-pic")
         if self.options.shared:
             args.extend(["--disable-static", "--enable-shared"])
@@ -66,6 +111,8 @@ class GPGErrorConan(ConanFile):
             args.extend(["--disable-shared", "--enable-static"])
 
         if self._is_msvc:
+            env["_LINK_"] = "advapi32.lib"
+            self._apply_msvc_fixes()
             # INSTALL.windows: Native binaries, built using the MS Visual C/C++ tool chain.
             for filename in ["compile", "ar-lib"]:
                 shutil.copy(os.path.join(self.deps_cpp_info["automake_build_aux"].rootpath, filename),
@@ -82,16 +129,20 @@ class GPGErrorConan(ConanFile):
                          "NM=dumpbin -symbols",
                          "STRIP=:",
                          "AR=$PWD/build-aux/ar-lib lib",
-                         "RANLIB=:"])
+                         "RANLIB=:",
+                         "gnupg_cv_mkdir_takes_one_arg=yes"])
             if rc:
                 args.extend(['RC=%s' % rc, 'WINDRES=%s' % rc])
 
         with tools.chdir(self._source_subfolder):
             with tools.vcvars(self.settings) if self._is_msvc else tools.no_op():
-                env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
-                env_build.configure(args=args, build=build, host=host)
-                env_build.make()
-                env_build.install()
+                with tools.environment_append(env):
+                    env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+                    if self._is_msvc:
+                        env_build.defines.extend(["strncasecmp=_strnicmp", "strcasecmp=_stricmp"])
+                    env_build.configure(args=args, build=build, host=host)
+                    env_build.make()
+                    env_build.install()
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
@@ -101,3 +152,5 @@ class GPGErrorConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["gpg-error"]
+        if self.settings.os == "Windows":
+            self.cpp_info.libs.append("ws2_32")
